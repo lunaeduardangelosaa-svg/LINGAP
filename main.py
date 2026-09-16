3a774e58f78bd2b4fad40dc215e465b7556729b4
lunaeduardangelosaa-svg <lunaeduardangelosaa@gmail.com>
2026-09-16 16:00:38 +0800

    Refactor main.py to remove camera functionality
    Removed camera and related handlers, simplified system initialization.

--- main.py
+++ main.py
@@ -1,65 +1,13 @@
-import cv2
-
-from camera_handler import CameraHandler
-from prescription_handler import PrescriptionHandler
-from rtc_handler import RTCHandler
-from fingerprint_handler import FingerprintHandler
-from reset_handler import ResetHandler
 from system_handler import SystemHandler
 
+# ============================================================
+# START LINGAP
+# ============================================================
 
-camera = CameraHandler()
-prescription = PrescriptionHandler()
-rtc = RTCHandler(prescription)
-fingerprint = FingerprintHandler()
-reset_handler = ResetHandler(fingerprint)
+system = SystemHandler()
 
-system = SystemHandler(
-    camera,
-    prescription,
-    fingerprint,
-    reset_handler,
-    rtc
-)
+system.run()
 
-camera.start()
-
-print()
-print("================================")
-print("          LINGAP SYSTEM")
-print("================================")
-print("F = ENROLL FINGERPRINT")
-print("G = FINGERPRINT LOGIN")
-print("H = RESET ALL DATA")
-print("E = ENROLL FACE")
-print("R = RECOGNIZE FACE")
-print("P = ADD PRESCRIPTION")
-print("L = LIST PEOPLE")
-print("Q = QUIT")
-print("================================")
-print()
-
-try:
-    while camera.running:
-        frame = camera.capture()
-
-        if frame is None:
-            continue
-
-        frame = system.process_frame(frame)
-
-        cv2.imshow(
-            "Face System",
-            frame
-        )
-
-        key = cv2.waitKey(1) & 0xFF
-
-        if system.handle_key(key):
-            break
-
-finally:
-    camera.stop()
-    fingerprint.close()
-    cv2.destroyAllWindows()
-    print("System stopped.")
+# ls /dev/ttyAMA* && dmesg | grep -i uart
+# espeak -v en-us+f3 -s 150 "your text"
+# http://127.0.0.1:5000
